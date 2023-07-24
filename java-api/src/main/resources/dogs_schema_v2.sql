5d09cb95257aafe0e5d1a4e655d4543f498e550c
George-Valentin Cujba <george.cujba15@gmail.com>
2023-07-24 15:37:25 +0100

    Create a View that contains the join between the dogs and owner table

--- java-api/src/main/resources/dogs_schema_v2.sql
+++ java-api/src/main/resources/dogs_schema_v2.sql
@@ -10,3 +10,10 @@ CREATE TABLE dogs (
     owner_id int not null,
     FOREIGN key (owner_id ) REFERENCES owners(owner_id)
 );
+
+CREATE VIEW dogs_and_owners AS
+    select d.name as "Dog Name", d.age as "Dog Age", o.name as "Owner Name"
+      from dogs as d join owners as o on
+    d.owner_id = o.owner_id;
+
+SELECT * FROM dogs_and_owners;
