01916da3599c7d09b99383a8a225874a64a36af4
Oana Butaru <oanabutaru98@gmail.com>
2023-07-24 15:12:21 +0100

    created and populated dogs table

---
+++ java-api/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dogs (name, age) VALUES
+('Pluto', 14),
+('Goofy', 21),
+('Nero', 35),
+('Bruno', 28),
+('Max', 42);
---
+++ java-api/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dogs (
+    id int auto_increment primary key,
+    name varchar (100) not null,
+    age int not null
+);
