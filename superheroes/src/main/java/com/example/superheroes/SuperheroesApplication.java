4c67b869164c9c49a937ec357b576e321141f095
Oana Butaru <oanabutaru98@gmail.com>
2023-07-28 14:17:22 +0100

    added ex 06

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -12,31 +12,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.List;
 
 @SpringBootApplication
-public class SuperheroesApplication implements CommandLineRunner {
+public class SuperheroesApplication
+		//implements CommandLineRunner
+{
 
 //	@Autowired
 //	Calculator calculator;
 //	@Autowired
 //	HeroRepository heroRepository;
-	@Autowired
-	HeroService heroService;
+//	@Autowired
+//	HeroService heroService;
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		//List<Hero> heroes = heroRepository.findAll();
-		//List<Hero> heroes = heroRepository.findTop3ByOrderByNameDesc();
-		//List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("d%");
-		//List<Hero> heroes = heroService.getHeroesStartingWithLetter("c");
-		//List<Hero> heroes = heroService.getAllHeroes();
-		List<Hero> heroes = heroService.getLast3HeroesByName();
-		for (Hero hero : heroes) {
-			System.out.println(hero);
-		}
-//		System.out.println("hello world");
-//		int result = calculator.addition(1,2);
-//		System.out.println(result);
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		//List<Hero> heroes = heroRepository.findAll();
+//		//List<Hero> heroes = heroRepository.findTop3ByOrderByNameDesc();
+//		//List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("d%");
+//		//List<Hero> heroes = heroService.getHeroesStartingWithLetter("c");
+//		//List<Hero> heroes = heroService.getAllHeroes();
+//		List<Hero> heroes = heroService.getLast3HeroesByName();
+//		for (Hero hero : heroes) {
+//			System.out.println(hero);
+//		}
+////		System.out.println("hello world");
+////		int result = calculator.addition(1,2);
+////		System.out.println(result);
+//	}
 }
