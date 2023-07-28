4c9cb09b4a421dbc571ad4f87cfd31d9f6e3eb6c
Oana Butaru <oanabutaru98@gmail.com>
2023-07-28 13:23:26 +0100

    added ex 04

--- superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ superheroes/src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -1,24 +1,36 @@
 package com.example.superheroes;
 
+import com.example.superheroes.model.Hero;
 import com.example.superheroes.practice.Calculator;
+import com.example.superheroes.repository.HeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SuperheroesApplication implements CommandLineRunner {
 
+//	@Autowired
+//	Calculator calculator;
 	@Autowired
-	Calculator calculator;
+	HeroRepository heroRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroesApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("hello world");
-		int result = calculator.addition(1,2);
-		System.out.println(result);
+		//List<Hero> heroes = heroRepository.findAll();
+		//List<Hero> heroes = heroRepository.findTop3ByOrderByNameDesc();
+		List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter("d%");
+		for (Hero hero : heroes) {
+			System.out.println(hero);
+		}
+//		System.out.println("hello world");
+//		int result = calculator.addition(1,2);
+//		System.out.println(result);
 	}
 }
--- superheroes/src/main/resources/schema.sql
+++ superheroes/src/main/resources/schema.sql
@@ -3,6 +3,6 @@ create table heroes (
     id int auto_increment primary key,
     name varchar(50) not null,
     alias varchar(50) not null,
-    superPower varchar(70) not null,
+    superpower varchar(70) not null,
     teamId int not null
 );
