b2a0d9da5dccc4bf4b697cd50971c07f0a3cbd13
oanabutaru <oanabutaru98@gmail.com>
2023-08-01 15:17:18 +0100

    controller changes

--- superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
+++ superheroes/src/main/java/com/example/superheroes/controller/HeroController.java
@@ -7,16 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HeroController {
     @Autowired
     HeroService heroService;
